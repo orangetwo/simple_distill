87c30d1e2d76ccb3cd4f2fc2b7f281fc355d6d50
lisi06 <lisi06@58.com>
2021-08-14 21:26:37 +0800

    修改tokenize.py和data_augment.py文件

---
+++ Tokenize.py
@@ -0,0 +1,157 @@
+from collections import Counter
+from typing import Union
+
+
+class Tokenizer:
+    def __init__(self, tokenizer, counter, max_size=None, min_freq=1,
+                 filter_token=(), specials=('<unk>', '<pad>'), lower=True):
+        """
+        Args:
+            max_size: The maximum size of the vocabulary, or None for no
+                    maximum. Default: None.
+            min_freq: The minimum frequency needed to include a token in the
+                vocabulary. Values less than 1 will be set to 1. Default: 1.
+            specials: The list of special tokens (e.g., padding or eos) that
+                will be prepended to the vocabulary. Default: ['<unk'>, '<pad>'].
+            tokenizer: tokenizer function, Default: None.
+            filter_token: filter the unuseful token. Default: ().
+            collections.Counter object holding the frequencies of
+                each value found in the data.
+        """
+
+        self.filter_token = filter_token
+        self.min_freq = max(min_freq, 1)
+        self.tokenizer = tokenizer
+        self.counter = counter
+        self.index_to_token = list()
+        self.specials = specials
+
+        self.index_to_token.extend(list(specials))
+        self.token_to_index = dict()
+
+        self.unk = specials[0]
+        self.max_size = max_size
+        self.lower = lower
+
+    def convert_sequence_to_tokens(self, sequence: str) -> list:
+        """
+        Args:
+            sequence: Input sequence.
+            if self.tokenizer is None, then using char-level.
+        """
+
+        if self.tokenizer is not None:
+            if self.lower:
+                return [token.lower() for token in self.tokenizer(sequence) if token not in self.filter_token]
+            else:
+                return [token for token in self.tokenizer(sequence) if token not in self.filter_token]
+        else:
+            if self.lower:
+                return [token.lower() for token in sequence if token not in self.filter_token]
+            else:
+                return [token for token in sequence if token not in self.filter_token]
+
+    def counter_sequences(self, sequences: list[str], save_words_and_frequencies=False):
+        """
+        Args:
+            sequences: The list of sequence.
+            save_words_and_frequencies: whether save the words & frequencies
+        """
+        if not isinstance(sequences, list):
+            raise ValueError('The input sequences should be a list of str!')
+
+        for sequence in sequences:
+            assert type(sequence) == str, f'sequence should be str type, not ' \
+                                          f'{type(sequence)} !!!'
+            self.counter.update(self.convert_sequence_to_tokens(sequence))
+
+        self.generate_vocab(save_words_and_frequencies=save_words_and_frequencies)
+
+    def generate_vocab(self, save_words_and_frequencies=False):
+        # sort by frequency, then alphabetically
+        for tok in self.specials:
+            del self.counter[tok]
+
+        words_and_frequencies = sorted(self.counter.items(), key=lambda tup: tup[0])
+        words_and_frequencies.sort(key=lambda tup: tup[1], reverse=True)
+
+        # TODO:
+        if save_words_and_frequencies:
+            pass
+
+        for word, freq in words_and_frequencies:
+            if self.max_size is not None:
+                if freq < self.min_freq or len(self.itos) == (self.max_size + len(self.specials)):
+                    break
+                self.index_to_token.append(word)
+            else:
+                # if the token freq less than self.min_freq, discard the token
+                if freq < self.min_freq:
+                    break
+                self.index_to_token.append(word)
+
+        self.token_to_index.update({tok: i for i, tok in enumerate(self.index_to_token)})
+
+    def convert_sentences_to_indices(self, sentences: Union[str, list[str]], unk='<unk>', seg=None):
+
+        if seg is not None:
+            tokenizer_sent = seg
+        else:
+            tokenizer_sent = self.tokenizer
+
+        if isinstance(sentences, str):
+            return [self.token_to_index.get(token, self.token_to_index[unk]) for token in tokenizer_sent(sentences)]
+
+        elif isinstance(sentences, list):
+            result = []
+
+            for sentence in sentences:
+                result.append(
+                    [self.token_to_index.get(token, self.token_to_index[unk]) for token in tokenizer_sent(sentence)])
+
+            return result
+        else:
+            raise ValueError('The input is neither a list nor a string!')
+
+    def convert_indices_to_sentences(self, indices: Union[list[int], list[list[int]]]):
+
+        assert len(indices) >= 1, 'The length of indices is less than 1!'
+
+        if isinstance(indices[0], int):
+            return [self.index_to_token[index] for index in indices]
+        elif isinstance(indices[0], list):
+            result = []
+            for indie in indices:
+                result.append([self.index_to_token[index] for index in indie])
+            return result
+        else:
+            raise ValueError('The input is neither a list of int nor a list of list!')
+
+    def __getitem__(self, token):
+
+        assert len(self.token_to_index) > 2, f'The current dictionary size is {len(self.token_to_index)}!!!'
+
+        if '<unk>' not in self.token_to_index:
+            print(f"Please check the '<unk> token'!")
+
+        return self.token_to_index.get(token, self.token_to_index.get(self.unk))
+
+    def __len__(self):
+        return len(self.index_to_token)
+
+
+def tokenizer(sentence):
+    return [token for token in sentence.split(' ')]
+
+
+if __name__ == '__main__':
+    counter = Counter()
+
+    vocab = Tokenizer(tokenizer=tokenizer, counter=counter)
+
+    vocab.counter_sequences(['不错 ， 下次 还 考虑 入住 。 交通 也 方便 ， 在 餐厅 吃 的 也 不错 。'])
+    # vocab.generate_vocab()
+
+    print(vocab.index_to_token)
+    print(vocab.counter)
+    print(vocab.token_to_index)
--- data_augment.py
+++ data_augment.py
@@ -1,5 +1,11 @@
+import collections
+import tokenize
+
 import jieba
 import numpy as np
+import torch
+
+from Tokenize import tokenizer,Tokenizer
 
 
 def ngram_sampling(words, p_ng=0.25, ngram_range=(2, 6)):
@@ -15,8 +21,11 @@ def ngram_sampling(words, p_ng=0.25, ngram_range=(2, 6)):
     return words
 
 
-def data_augmentation(dataPath, p_mask, p_ng, ngram_range=(2,6), n_iter=20,tokenizer=jieba.lcut, min_length=2):
-
+def data_augmentation(dataPath, p_mask=0.75, p_ng=0.25, ngram_range=(2, 6), n_iter=20, tokenizer=jieba.lcut,
+                      min_length=2):
+    """
+    generate data augment train set.
+    """
     input4student_input4bert_label = []
 
     with open(dataPath, 'r', encoding='utf-8') as fr:
@@ -49,12 +58,52 @@ def data_augmentation(dataPath, p_mask, p_ng, ngram_range=(2,6), n_iter=20,token
     return input4student_input4bert_label
 
 
-def get_w2v():
+def get_w2v(tokens=None):
     """
     load the word embedding.
     """
     for line in open('data/cache/word2vec', encoding="utf-8").read().strip().split('\n'):
         line = line.strip().split()
-        if not line:
+        if not line:  # or line[0] not in tokens:
             continue
         yield line[0], np.array(list(map(float, line[1:])))
+
+
+if __name__ == '__main__':
+    path = './data/hotel/DA.txt'
+    # x = data_augmentation(path, n_iter=10)
+    # for y in x:
+    #     print(y)
+    print({0: '<unk>', 1: '不错', 2: '，', 3: '。', 4: '也', 5: '下次', 6: '还', 7: '考虑', 8: '入住', 9: '交通', 10: '方便', 11: '在',
+           12: '餐厅', 13: '吃', 14: '的'})
+    print({'<unk>': 0, '不错': 1, '，': 2, '。': 3, '也': 4, '下次': 5, '还': 6, '考虑': 7, '入住': 8, '交通': 9, '方便': 10, '在': 11,
+           '餐厅': 12, '吃': 13, '的': 14})
+    tokenizer = tokenizer
+
+    counter = collections.Counter()
+    vocab = Tokenizer(tokenizer=tokenizer, counter=counter)
+    texts = [' '.join(jieba.cut(line.split('\t', 1)[1].strip())) for line in open(path, encoding="utf-8",).read().strip().split('\n')]
+
+    from torch import nn
+
+    embeds = nn.Embedding(2, 5)  # 2 个单词，维度 5
+
+    print(embeds.weight.data)
+    print(type(embeds.weight.data))
+    embeds.weight.data[0] = torch.FloatTensor([1.1820, 0.0601, 0.1449, 0.4057, 1.3008])
+
+    print(embeds.weight.data)
+
+    print({0: '<unk>', 1: '不错', 2: '，', 3: '。', 4: '也', 5: '下次', 6: '还', 7: '考虑', 8: '入住', 9: '交通', 10: '方便', 11: '在',
+           12: '餐厅', 13: '吃', 14: '的'})
+    print({'<unk>': 0, '不错': 1, '，': 2, '。': 3, '也': 4, '下次': 5, '还': 6, '考虑': 7, '入住': 8, '交通': 9, '方便': 10, '在': 11,
+           '餐厅': 12, '吃': 13, '的': 14})
+    print(texts)
+    vocab.counter_sequences(texts)
+
+    print(vocab.index_to_token)
+    print(vocab.token_to_index)
+
+    print(vocab.convert_sentences_to_indices(texts))
+    indices = [[3, 5, 6, 14, 13, 8, 2, 7, 4, 11, 5, 10, 15, 9, 12, 4, 3, 2]]
+    print(vocab.convert_indices_to_sentences(indices))
