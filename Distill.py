0760f27845fc9edcab161ce899ec038362c9f3cf
orange <leesifox@gmail.com>
2021-08-18 00:47:27 +0800

    修复路径

--- Distill.py
+++ Distill.py
@@ -9,6 +9,7 @@ from Tokenize import TokenizerX
 from utils import char_tokenizer, convert_sample_to_indices, prepare_data, Mydataset, collate_fn
 
 train_path = './data/train.txt'
+test_path = './data/test.txt'
 counter = collections.Counter()
 n_iter = 10
 
@@ -18,18 +19,17 @@ texts = [line.split('\t', 1)[1].strip() for line in open(train_path, encoding="u
 vocab.counter_sequences(texts)
 
 # if you want to update the vocab by the test set you can use following code.
-
 # texts = [line.split('\t', 1)[1].strip() for line in open(test_file_path, encoding="utf-8",
 #                                                          ).read().strip().split('\n')]
 # vocab.counter_sequences(texts)
 
-# update vocab
+# if '[MASK] in vocab, delete it.'
 vocab.update_vocab(add_tokens={}, discard_tokens={'[MASK]'})
 
-test_path = './data/hotel/train.txt'
-train_raw = data_augmentation(path, n_iter=n_iter, tokenizer=char_tokenizer)
 
-path = './data/hotel/test.txt'
+train_raw = data_augmentation(train_path, n_iter=n_iter, tokenizer=char_tokenizer)
+
+path = './data/test.txt'
 test_raw = data_augmentation(path, n_iter=0, tokenizer=char_tokenizer)
 
 bertTokenizer = BertTokenizer.from_pretrained('./bert-base-chinese')
